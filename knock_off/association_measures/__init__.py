2844d3cb07a204ba351a6721b720666ef559c926
Héctor Climente-González <hclimente@gmail.com>
2021-09-10 23:27:57 +0200

    added pearson correlation

--- knock_off/association_measures/__init__.py
+++ knock_off/association_measures/__init__.py
@@ -2,8 +2,9 @@ from .projection_correlation import projection_corr
 from .tr import tr
 from .hsic import HSIC
 from .mmd import MMD
+from .pearson_correlation import pearson_correlation
 from .distance_correlation import distance_corr
 
 
 # https://github.com/vnmabus/dcor
-__all__ = ["projection_corr", "tr", "distance_corr", "HSIC", "MMD"]
+__all__ = ["projection_corr", "tr", "distance_corr", "HSIC", "MMD", "pearson_correlation"]
