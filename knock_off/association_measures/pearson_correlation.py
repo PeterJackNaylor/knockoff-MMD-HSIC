c5a98aade5eb7f64695e75ac918f3dde058d4e91
PeterJackNaylor <peter.jack.naylor@gmail.com>
2021-09-13 22:04:57 +0200

    making pearson d dimensional

--- knock_off/association_measures/pearson_correlation.py
+++ knock_off/association_measures/pearson_correlation.py
@@ -2,7 +2,14 @@ import numpy as np
 from scipy.stats import pearsonr
 
 def pearson_correlation(X, Y):
+    n, d = X.shape
+    ny, nd = Y.shape
 
-    X = np.squeeze(X, 1)
+    assert n == ny
+    assert nd == 1
+    pearson_stats = np.zeros((d, 1))
+    for i in range(d):
+        pearson_stats[i] = np.array([np.float32(pearsonr(X[:, i], Y[:, 0])[0])])
 
-    return np.array([np.float32(pearsonr(X, Y)[0])])
+
+    return pearson_stats
