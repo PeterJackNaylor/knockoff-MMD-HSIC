a196af7f20870c30145ae670a64fdd9036b17b6d
PeterJackNaylor <peter.jack.naylor@gmail.com>
2021-09-15 15:01:09 +0200

    fixed selected features -- overwriting fdr.csv -- useless variables

--- src/model/knock-off.py
+++ src/model/knock-off.py
@@ -24,7 +24,6 @@ def false_discovery_rate(selected_features, dataset="model_2a"):
         return -1
     else:
         n_selected = len(selected_features)
-        correct_covariates = [0, 1, 2, 3]
         intersection = list(set(selected_features) & set(correct_covariates))
         number_of_correct_positives = len(intersection)
         fdr = (n_selected - number_of_correct_positives) / n_selected
@@ -35,17 +34,17 @@ def f_fdp(t, w):
     den = max((w >= t).sum(), 1)
     return num / den 
 
-def false_discovery_proportion(n_features_out, wjs):
+# def false_discovery_proportion(n_features_out, wjs):
 
-    if n_features_out == 0:
-        print("No feature selection process happened")
-        return [None], [None]
-    else:
-        t_s = wjs.copy()
-        t_s = [abs(w) for w in t_s]
-        t_s.sort()
-        fdp = [f_fdp(t, wjs) for t in t_s]
-        return t_s, fdp
+#     if n_features_out == 0:
+#         print("No feature selection process happened")
+#         return [None], [None]
+#     else:
+#         t_s = wjs.copy()
+#         t_s = [abs(w) for w in t_s]
+#         t_s.sort()
+#         fdp = [f_fdp(t, wjs) for t in t_s]
+#         return t_s, fdp
 
 def options():
     # options
@@ -85,19 +84,16 @@ def main():
     pd_fdr = DataFrame(columns=opt.param_d.keys(), index=list(range(10, 95, 5)))
 
     for alpha in range(10, 95, 5):
-        alpha_ind, t_alpha, n_feat = model.alpha_threshold(alpha / 100)
-        t_s, fdp = false_discovery_proportion(n_feat, model.wjs_)
+        alpha_ind, _, _ = model.alpha_threshold(alpha / 100)
+        # t_s, fdp = false_discovery_proportion(n_feat, model.wjs_)
         fdr = false_discovery_rate(alpha_ind, dataset=opt.param_d["DATASET"])
         print(f"False discovery rate = {fdr} for alpha = {alpha}")
         opt.param_d["fdr"] = fdr
         opt.param_d["alpha"] = alpha / 100
         pd_fdr.loc[alpha] = Series(opt.param_d)
     
-    
+    # Record fdr and report parameters    
     pd_fdr.to_csv("fdr.csv", index=False)
-    ## Record fdr and report parameters
-    opt.param_d["fdr"] = fdr
-    DataFrame(opt.param_d, index=[0]).to_csv("fdr.csv", index=False)
 
 if __name__ == "__main__":
     main()
